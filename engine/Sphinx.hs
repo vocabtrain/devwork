7f2ee5a53a7476c851a2659f0f51a1f865122d01
Dominik Köppl <niki@berlios.org>
2013-01-27 10:00:19 +0100

    Before change of cardsheet

---
+++ engine/Sphinx.hs
@@ -0,0 +1,20 @@
+module Sphinx where
+
+import Prelude (map)
+import qualified Text.Search.Sphinx as Sphinx
+import qualified Text.Search.Sphinx.Types as SphinxT
+import Import
+import GHC.Int
+
+querySphinx :: Text -> Text -> IO [Int64]
+querySphinx lang queryStr = do
+	result <- liftIO $ Sphinx.query config lang queryStr
+	case result of
+		SphinxT.Ok res -> return $ map SphinxT.documentId $ SphinxT.matches res
+		_ -> return []
+
+		where config = Sphinx.defaultConfig {
+			Sphinx.port = 9312
+		,	Sphinx.mode = SphinxT.Any
+		}
+
